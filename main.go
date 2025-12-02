90f1dbebf8e074e7e73dfa9ab0f80a20c7d23d70
jorge-ld8 <jaleon.19@est.ucab.edu.ve>
2025-12-02 08:46:26 -0400

    add/list methods and file persistence

--- main.go
+++ main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
 
-func main(){
-	fmt.Println("Welcome to the TODO CLI application!")
+	"github.com/jorge-ld8/todo-cli/todo"
+)
+
+func main() {
+	// three main parts
+
+	// if len < 2 exit (general error checking)
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	// load todos or try to, if error exit
+	todos := &todo.Todos{}
+	if err := todos.Load(); err != nil {
+		fmt.Println("Error loading todos:", err)
+		os.Exit(1)
+	}
+
+	// switch on os.args[1]
+	switch os.Args[1] {
+	case "add":
+		cmdAdd(todos, os.Args[2:])
+	case "list":
+		cmdList(todos)
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+// cmd add 
+func cmdAdd(todos *todo.Todos, args []string) {
+	if len(args) == 0 {
+		fmt.Println("Provide a title for the todo item.")
+		os.Exit(1)
+	}
+
+	title := strings.Join(args, " ")
+	item := todos.Add(title)
+
+	if err := todos.Save(); err != nil {
+		fmt.Println("Error saving todo:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Added todo: %d - %s\n", item.ID, item.Title)
+}
+
+// cmd list
+func cmdList(todos *todo.Todos) {
+	// bring the list of todos (from todo/todo.go)
+	items := todos.List()
+
+	if len(items) == 0 {
+		fmt.Println("No todo items added. Add one with 'todo add <task>'")
+		return
+	}
+
+	fmt.Println("\nTodo List:")
+	fmt.Println(strings.Repeat("-", 40))
+
+	// iterate over todos list 
+	for _, item := range items {
+		status := "[ ]"
+		if item.Completed {
+			status = "[✓]"
+		}
+		fmt.Printf("%s %d: %s\n", status, item.ID, item.Title)
+	}
+	fmt.Println()
+}
+
+//if you typed an unknown command print usage manual
+func printUsage() {
+	fmt.Println(`Todo CLI - A simple task manager
+Usage:
+  todo <command> [arguments]
+
+Commands:
+  add <title>      Add a new todo
+  list             List all todos`)
 }
--- todo/todo.go
+++ todo/todo.go
@@ -1,6 +1,10 @@
 package todo
 
-import "time"
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
 
 // Item represents a single todo item.
 type Item struct {
@@ -14,4 +18,61 @@ type Item struct {
 // Todos represents a collection of todo items.
 type Todos struct {
 	Items []Item `json:"items"`
+}
+
+
+// add todo item
+func (t *Todos) Add(title string) Item {
+	newID := 1
+	if len(t.Items) > 0 {
+		newID = t.Items[len(t.Items)-1].ID + 1
+	}
+
+	// create the item
+	item := Item{
+		ID:		newID,
+		Title:	title,
+		Completed:	false,
+		CreatedAt:	time.Now(),
+	}
+
+	// append the item
+	t.Items = append(t.Items, item)
+
+	// return the item
+	return item
+}
+
+// get list of todo items
+func (t *Todos) List() []Item {
+	return t.Items
+}
+
+// FILE PERSISTENCE methods
+const defaultFile string = ".todos.json"
+
+// save a file
+func (t *Todos) Save() error {
+	data, err := json.MarshalIndent(t, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(defaultFile, data, 0644)
+}
+
+// load from file
+func (t *Todos) Load() error {
+	data, err := os.ReadFile(defaultFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// No existing file, start with empty todos
+			t.Items = []Item{}
+			return nil
+		}
+		return err
+	}
+	
+	return json.Unmarshal(data, t)
 }
